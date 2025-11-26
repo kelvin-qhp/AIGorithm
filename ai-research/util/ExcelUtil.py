7aba25fb064885840d4880ff88fbcb9976e61c48
Kelvin Qin <kelvinqin@globalsources.com>
2025-11-26 10:00:08 +0800

    v2.2 add csv & pickle util

--- ai-research/util/ExcelUtil.py
+++ ai-research/util/ExcelUtil.py
@@ -5,3 +5,7 @@ class ExcelUtil:
         df = pd.read_excel(fileName,sheet_name =sheetName)
         print(f'Success to read file：{fileName} size:{df.shape}')
         return df
+    def readCsv(fileName,sheetName=None):
+        df = pd.read_csv(fileName)
+        print(f'Success to read file：{fileName} size:{df.shape}')
+        return df
--- ai-research/util/jsonUtil.py
+++ ai-research/util/jsonUtil.py
@@ -1,4 +1,5 @@
 import json
+import pickle
 
 def saveJson(filePath,data):
     with open(filePath, 'w', encoding='utf-8') as f:
@@ -7,3 +8,11 @@ def saveJson(filePath,data):
 def readJson(filePath):
     with open(filePath, 'r', encoding='utf-8') as f:
         return json.load(f)
+
+def saveJson2(filePath,data):
+    with open(filePath, 'wb') as f:
+        pickle.dump(data, f)
+
+def readJson2(filePath):
+    with open(filePath, 'rb') as f:
+        return pickle.load(f)
