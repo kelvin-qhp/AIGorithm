0d555ff00f7a17c6360647c8dda7cf3c515db52e
Kelvin Qin <kelvinqin@globalsources.com>
2026-06-17 14:22:35 +0800

    sentence transformers embedding

--- ai-research/embedding/mpnet_embedding.py
+++ ai-research/embedding/mpnet_embedding.py
@@ -19,30 +19,35 @@ def transformer_embedding(sentences):
     model = AutoModel.from_pretrained(MODEL_BASE_PATH,trust_remote_code=False)
 
     # Tokenize sentences
-    encoded_input = tokenizer(sentences, padding=True, truncation=True, return_tensors='pt')
+    encoded_input = tokenizer(sentences, padding=True, truncation=True, return_tensors='pt', max_length=512)
 
     # Compute token embeddings
     with torch.no_grad():
         model_output = model(**encoded_input)
 
-    # Perform pooling
-    sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
-    print(f'embedding verctor sharp size:{sentence_embeddings.shape}')
-    # Normalize embeddings
-    sentence_embeddings = F.normalize(sentence_embeddings, p=2, dim=1)
+    token_embeddings = model_output.last_hidden_state
+    attention_mask = encoded_input['attention_mask']
 
-    print(f"Sentence embeddings-{sentence_embeddings.shape}:{sentence_embeddings}")
+    mask = attention_mask.unsqueeze(-1).float()
+    sum_embeddings = (token_embeddings * mask).sum(dim=1)
+    sum_mask = mask.sum(dim=1)
+    embeddings = sum_embeddings / sum_mask
+
+    # Normalize embeddings
+    sentence_embeddings = F.normalize(embeddings, p=2, dim=1)
+
+    print(f"transformer embeddings-{sentence_embeddings.shape}:{sentence_embeddings.numpy()}")
 
 def sentence_transformers_embedding(sentences):
 
-    embeddings = model.encode(sentences,convert_to_tensor=True)
-    print(f"sentence_transformers embeddings-{embeddings.shape}:{embeddings}")
+    embeddings = model.encode(sentences,convert_to_tensor=True, convert_to_numpy=True,normalize_embeddings=True)
+    print(f"sentence_transformers embeddings-{embeddings.shape}:{embeddings.numpy()}")
     return embeddings
 
 if __name__ == '__main__':
     model = SentenceTransformer(MODEL_BASE_PATH)
     # Sentences we want sentence embeddings for
-    sentences = ["I love Python programming","I love Python programming","I love Python","I dont like Python",
+    sentences = ["I love Python programming",
     "Python is my favorite language",
     "I enjoy coding in Java",
     "I hate python programming",
@@ -55,19 +60,19 @@ if __name__ == '__main__':
     corpus_embeddings = sentence_transformers_embedding(sentences)
 
     query_sentence = "I like Python"
-    query_sentence_embeddings = sentence_transformers_embedding(query_sentence)
+    query_sentence_embeddings = transformer_embedding(sentences)
 
-    similarities = util.pytorch_cos_sim(query_sentence_embeddings, corpus_embeddings)[0]
-    top_results = torch.topk(similarities, k=5)
-
-    for idx, score in zip(top_results.indices, top_results.values):
-        print(f"  {score:.3f} - {sentences[idx]}")
-
-    top_results1 = util.semantic_search(query_sentence_embeddings, corpus_embeddings, top_k=5)
-    for result in top_results1[-1]:
-        print(f'{result['corpus_id']}:{result['score']:.3f} -sentence:{sentences[result['corpus_id']]}')
-
-    pairs = util.paraphrase_mining_embeddings(corpus_embeddings, top_k=3)
-    print(pairs)
-    for score, idx1, idx2 in pairs:
-        print(f"相似度: {score:.4f}  [{idx1}] - {sentences[idx1]} & [{idx2}] {sentences[idx2]}")
+    # similarities = util.pytorch_cos_sim(query_sentence_embeddings, corpus_embeddings)[0]
+    # top_results = torch.topk(similarities, k=5)
+    #
+    # for idx, score in zip(top_results.indices, top_results.values):
+    #     print(f"  {score:.3f} - {sentences[idx]}")
+    #
+    # top_results1 = util.semantic_search(query_sentence_embeddings, corpus_embeddings, top_k=5)
+    # for result in top_results1[-1]:
+    #     print(f'{result['corpus_id']}:{result['score']:.3f} -sentence:{sentences[result['corpus_id']]}')
+    #
+    # pairs = util.paraphrase_mining_embeddings(corpus_embeddings, top_k=3)
+    # print(pairs)
+    # for score, idx1, idx2 in pairs:
+    #     print(f"相似度: {score:.4f}  [{idx1}] - {sentences[idx1]} & [{idx2}] {sentences[idx2]}")
